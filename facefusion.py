f6f1378f091941b86904d15f44918c1c5081f022
jayeAch <133476303+jayeAch@users.noreply.github.com>
2025-06-15 13:23:19 -0400

    Update facefusion.py

--- facefusion.py
+++ facefusion.py
@@ -2,7 +2,10 @@
 
 import os
 
-os.environ['OMP_NUM_THREADS'] = '1'
+# Set optimal threading environment variables
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'  # Avoid library conflicts
+os.environ['OMP_NUM_THREADS'] = '2'           # OpenMP threads
+os.environ['ONNX_NUM_THREADS'] = '2'          # ONNX Runtime threads
 
 from facefusion import core
 
